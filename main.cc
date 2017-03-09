36599b8c78d012e7fcd0cf5c18509935e150a353
Чернышева Анастасия <ulyanova.27@yandex.ru>
2017-03-09 14:38:50 +0300

    внесены изменения в программу

--- main.cc
+++ main.cc
@@ -1,7 +1,13 @@
 //
 // Created by u211-12 on 09.03.2017.
 //
-int main()
-{
-    return 0;
-}
+#include <iostream>
+using namespace std;
+
+     int main()
+    {
+         int a, b;
+         cout << "Enter A and B: ";
+         cin >> a >> b;
+    }
+
